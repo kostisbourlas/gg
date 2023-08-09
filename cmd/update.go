7f7629f4d9ada19780f804a17beaa519792e2bf1
Kostis Bourlas <kostisbourlas@protonmail.com>
2023-08-09 20:31:51 +0300

    Add multiple paths when use update command

--- cmd/update.go
+++ cmd/update.go
@@ -20,39 +20,43 @@ var updateCmd = &cobra.Command{
 }
 
 func updateRun(cmd *cobra.Command, args []string) {
-	path, _ := cmd.Flags().GetString("path")
+	var paths []string
+	for _, arg := range args {
+		paths = append(paths, arg)
+	}
 	branch, _ := cmd.Flags().GetString("branch")
 
-	isGitRepo := Git.IsGitRepository(path)
-	if isGitRepo == false {
-		fmt.Println("Path is not a git repository.")
-		return 
+	for _, path := range paths {
+		isGitRepo := Git.IsGitRepository(path)
+		if isGitRepo == false {
+			fmt.Println("Path is not a git repository.")
+			return 
+		}
+	
+		current_branch := Git.GetCurrentBranch(path)
+	
+		err := Git.CheckoutToBranch(path, branch)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	
+		err = Git.UpdateGitRepository(path)
+		if err != nil {
+			fmt.Println(err)
+			return 
+		}
+	
+		err = Git.CheckoutToBranch(path, current_branch)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Update ran successfully!")
 	}
-
-	current_branch := Git.GetCurrentBranch(path)
-
-	err := Git.CheckoutToBranch(path, branch)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	err = Git.UpdateGitRepository(path)
-	if err != nil {
-		fmt.Println(err)
-		return 
-	}
-
-	err = Git.CheckoutToBranch(path, current_branch)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("Update ran successfully!")
 }
 
 func init() {
 	rootCmd.AddCommand(updateCmd)
-	updateCmd.Flags().StringP("path", "p", "", "specific directory")
 	updateCmd.Flags().StringP("branch", "b", "master", "specific git branch")
 }
