a8859c59d8b54ec8ca9acaa9313544ab41ecd696
Kostis Bourlas <kostisbourlas@protonmail.com>
2023-08-12 14:56:15 +0300

    Add print statements before execute a git command

--- git/args.go
+++ git/args.go
@@ -1,6 +1,9 @@
 package git
 
+import "fmt"
+
 func getGitPullRebaseArgs(args ...string) []string {
+	fmt.Println("Pulling from remote branch...")
 	path := args[0]
 	return []string{"git", "-C", path, "pull", "--rebase"}
 }
@@ -8,15 +11,18 @@ func getGitPullRebaseArgs(args ...string) []string {
 func getGitCheckoutToBranchArgs(args ...string) []string {
 	path := args[0]
 	branch := args[1]
+	fmt.Printf("Switching to branch '%s'...\n", branch)
 	return []string{"git", "-C", path, "checkout", branch}
 }
 
 func getGitStashArgs(args ...string) []string {
+	fmt.Println("Stashing changes...")
 	path := args[0]
 	return []string{"git", "-C", path, "stash"}
 }
 
 func getGitStashPopArgs(args ...string) []string {
+	fmt.Println("Poping changes back...")
 	path := args[0]
 	return []string{"git", "-C", path, "stash", "pop"}
 }
