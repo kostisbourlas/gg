1c53ea1e419958fccd7a0235fea2859b7a0d074a
Kostis Bourlas <kostisbourlas@protonmail.com>
2023-08-12 13:05:30 +0300

    Rename values in git commands

--- git/commands.go
+++ git/commands.go
@@ -7,27 +7,27 @@ import (
 
 func performGitStashPop(path string) ([]byte, error) {
 	fmt.Println("Performing git stash pop...")
-	gitStashPopArgs := getGitStashPopArgs(path)
-	gitStashPopCmd := exec.Command(gitStashPopArgs[0], gitStashPopArgs[1:]...)
-	output, err := gitStashPopCmd.CombinedOutput()
+	cmdArgs := getGitStashPopArgs(path)
+	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
+	output, err := cmd.CombinedOutput()
 	fmt.Printf("%s\n", output)
 	return output, err
 }
 
 func performGitStash(path string) ([]byte, error) {
 	fmt.Println("Performing git stash...")
-	gitStashArgs := getGitStashArgs(path)
-	gitStashCmd := exec.Command(gitStashArgs[0], gitStashArgs[1:]...)
-	output, err := gitStashCmd.CombinedOutput()
+	cmdArgs := getGitStashArgs(path)
+	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
+	output, err := cmd.CombinedOutput()
 	fmt.Printf("%s\n", output)
 	return output, err
 }
 
 func performGitPull(path string) ([]byte, error) {
 	fmt.Println("Performing git pull --rebase...")
-	gitPullRebaseArgs := getGitPullRebaseArgs(path)
-	gitPullRebaseCmd := exec.Command(gitPullRebaseArgs[0], gitPullRebaseArgs[1:]...)
-	output, err := gitPullRebaseCmd.CombinedOutput()
+	cmdArgs := getGitPullRebaseArgs(path)
+	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
+	output, err := cmd.CombinedOutput()
 	fmt.Printf("%s\n", output)
 	return output, err
 }
@@ -49,8 +49,8 @@ func performGitCurrentBranch(path string) ([]byte, error) {
 func performGitCheckoutToBranch(path string, branch string) ([]byte, error) {
 	fmt.Printf("Performing git checkout to %s...\n", branch)
 	cmdArgs := getGitCheckoutToBranchArgs(path, branch)
-	gitCheckoutCommand := exec.Command(cmdArgs[0], cmdArgs[1:]...)
-	output, err := gitCheckoutCommand.CombinedOutput()
+	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
+	output, err := cmd.CombinedOutput()
 	fmt.Printf("%s\n", output)
 	return output, err
 }
