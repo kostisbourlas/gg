6c76ba7ca6c1cda12d2a69346ea9d2cf6f23df5f
Kostis Bourlas <kostisbourlas@protonmail.com>
2023-08-12 18:59:27 +0300

    Make commands output colorized like in terminal

--- git/abstraction.go
+++ git/abstraction.go
@@ -10,7 +10,7 @@ type gitParams func(...string) []string
 func executeGitCommand(getGitCommandParams gitParams, args ...string) ([]byte, error) {
 	cmdArgs := extractGitArgs(getGitCommandParams, args)
 	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
-	output, err := cmd.CombinedOutput()
-	fmt.Printf("%s\n", output)
-	return output, err
+	outputBytes, err := executeCmdInPseudoTerminal(cmd)
+	fmt.Printf("%s", outputBytes)
+	return outputBytes, err
 }
--- git/utils.go
+++ git/utils.go
@@ -1,6 +1,10 @@
 package git
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/creack/pty"
+	"os/exec"
+)
 
 func extractGitArgs(getGitCommandParams gitParams, args []string) []string {
 	var cmdArgs []string
@@ -12,4 +16,26 @@ func extractGitArgs(getGitCommandParams gitParams, args []string) []string {
 		fmt.Println("Invalid number of parameters. Give one or two.")
 	}
 	return cmdArgs
+}
+
+func executeCmdInPseudoTerminal(cmd *exec.Cmd) ([]byte, error) {
+	ptmx, err := pty.Start(cmd)
+	if err != nil {
+		return nil, err
+	}
+	defer ptmx.Close()
+
+	outputBytes := make([]byte, 0)
+	for {
+		var n int
+		buf := make([]byte, 4096)
+		n, err = ptmx.Read(buf)
+		if err != nil {
+			break
+		}
+		outputBytes = append(outputBytes, buf[:n]...)
+	}
+
+	err = cmd.Wait()
+	return outputBytes, err
 }
